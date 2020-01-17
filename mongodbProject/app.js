922d100b31a4348f23663e07ca5f09c01f659f80
LEEJUNB <topguide82@gmail.com>
2020-01-17 23:41:25 +0900

    process login pass routing

--- mongodbProject/app.js
+++ mongodbProject/app.js
@@ -40,6 +40,83 @@ app.use(expressSession({
     saveUninitialized:true
 }));
 
+// Use MongoDB module
+var MongoClient = require('mongodb').MongoClient;
+
+// Declare variables for Database's objects
+var database;
+
+// Use mongodb module to connect the mongodb database
+function connectDB() {
+    // DB connect inf
+    var databaseUrl = 'mongodb://localhost:27017/local';
+
+    // DB connect
+    MongoClient.connect(databaseUrl, function(err,db){
+        if(err) throw err;
+
+        console.log('DB connect : ' + databaseUrl);
+        
+        // database 변수에 할당
+        database = db;
+    });
+}
+
+//===== start server =====//
+http.createServer(app).listen(app.get('port'),function(){
+    console.log('start the server. port : ' + app.get('port'));
+
+    //connection database
+    connectDB();
+});
+
+//===== compare id to password
+// Function that authenticates the user
+var authUser = function(database, id, password, callback) {
+    console.log('authUser 호출됨.');
+
+    // users 컬렉션 참조
+    var users = database.collection('users');
+
+    // 아이디 및 비번 사용 통한 검색
+    users.find({"id":id,"password":password}).toArray(function(err,docs){
+        if(err) {
+            callback(err,null);
+            return;
+        }
+        if(docs.length > 0){
+            console.log('아이디 [%s], 비밀번호 [%s]가 일치하는 사용자 찾음.', id, password);
+            callback(null, docs);
+        } else { 
+            console.log("일치하는 사용자를 찾지 못함");
+            callback(null,null);
+        }
+    });
+}
+
+// 로그인 처리 요청한 패스에 라우팅 함수 추가
+app.post('/process/login', function(req,res){
+    console.log('/process/login 호출');
+
+    var paramId = req.param('id');
+    var paramPassword = req.param('password');
+
+    if(database) {
+        authUser(database, paramId, paramPassword, function(err, docs){
+            if(err) {throw err;}
+
+            if(docs) {
+                console.dir(docs);
+            } else {
+
+            }
+        });
+    } else {
+
+    }
+});
+
+
 const router = express.Router(); // 라우터객체참조
 
 // login routing function
